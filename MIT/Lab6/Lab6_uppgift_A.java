31cec9b6b9b971be078203c9548b8ec4c921db9c
h-kan <h-kan@neomi.(none)>
2009-03-16 21:29:55 +0100

    uppdaterad lab6

--- MIT/Lab6/Lab6_uppgift_A.java
+++ MIT/Lab6/Lab6_uppgift_A.java
@@ -1,3 +1,4 @@
+
 /*
  * Håkan Gullstrand
  * 12-03-05
@@ -16,51 +17,49 @@ public class Lab6_uppgift_A {
 		BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
 
 
-		int i=0; //räknare för while loopen
-		int j=0; //räknare för arrayn
-		int maxTal=0; 
-		int minTal=0;
-		int totalValue=0;
-		int [] nummer = new int [20]; //Skappa en array med 20 indexeringar
-		int intal=0;
-		while(i<20)
-		{	
-			System.out.println("Mata in ett tal : ");
-
-			try {
-				intal = Integer.parseInt(stdin.readLine()); // läser in användarens tal
-				nummer[j] = intal;  //lagrar användarens tal i arrayn
-				j++; //stegar upp ett index i arrayn
+		int i=0; //räknare för uppräkning under inmatning
+		int minTal=Integer.MAX_VALUE; //variabel för minsta talet som just nu innehåller det högsta talet en int kan ha och då blir vilket tal som helt lägre
+		int maxTal=Integer.MIN_VALUE; //variabel för högsta inmatade talet
+		int totalValue=0; // varaibel för sammanlagda summan av inmatade tal
+		int nums[][] = new int[4][5]; // array
+		int intal=0; // variabaler för inamated talet
+		
+		for (int col=0; col<4; col++)
+			for (int rad=0; rad<5; rad++) {
+				System.out.println("Mata in tal " + (i++) + " : "); // fråga efter ett tal och skriv ut i så vi vet hur många gånger vi frågat
+				try 
+				{
+					intal = Integer.parseInt(stdin.readLine()); // läser in användarens tal
+					nums[col][rad]=intal;
 				} 
-			catch(NumberFormatException e) { // om val inte är ett giltligt värde så kör detta
-				System.out.println("Inamatningen är ej ett tal!!!");
+				catch(NumberFormatException e) { // om val inte är ett giltligt värde så kör detta
+					System.out.println("Inamatningen är ej ett tal!!!");
 				}
 			
-			if(i==0)
-			{
-				minTal=intal;
-				maxTal=intal;
-			}
-			else
-			{
 				if (intal<minTal) // om det inmatade talet är mindre än minTal variabeln så lagra det 
 					minTal= intal;
 				if (intal> maxTal) // om det inmatade taler är större än maxTal variabeln så lagra det
 					maxTal = intal;
+				
+				totalValue+=intal; // adera det inmatade talet i totalVärde variabeln
 			}
-			totalValue=intal; // lägg till det inmatade talet i totalVärde variabeln
-			i++; //räkna upp vår räknare med 1
-		}
-		int medelValue = totalValue/20; //lagra medelvärdet i variabeln
+		
+		float medelValue = totalValue/20; //lagra medelvärdet i variabeln
 
 		System.out.println("Högsta talet var : \t"+maxTal);
 		System.out.println("Minsta talet var : \t"+minTal);
 		System.out.println("Medel värdet var : \t"+medelValue);
 
-		for (int k = 0; k < nummer.length;)  //så länge k är mindre än längden på vår array så loopa igenom nedanstående
-		      System.out.print(nummer[k]+ ((++k%5)==0?"\n":"\t ") );
-		    
-		  }
-	}
+		for (int k = 0; k < 4; k++) //så länge k är mindre än 4 så kör nedanstående 
+		{
+			for (int m=0;m<5;m++) //så länge m är mindre än 5 så skriv ut innehlållet i vår array, när 5 nås så skriv ut en radbrytning
+			{
+				System.out.print(nums[k][m]+"\t ");
+			}
+			System.out.print("\n");
+			}
+		}
+}
+
 
 
